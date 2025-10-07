92d76b7884b006745a9d2ba11fd385bb6e3a80d1
Liu <Unofatira@gmail.com>
2025-10-07 16:32:34 +0700

    add

---
+++ Getskill/absen.java
@@ -0,0 +1,20 @@
+public class absen {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println();
+            System.out.println("kelas" + i);
+            for (int j = 1; j <= 10; j++) {
+                
+                if (j == 8) {
+                    break;
+                }
+                
+                if (j == 3) {
+                    continue;
+                }
+                
+                System.out.println("siswa ke-" + j + " hadir");
+            }
+        }
+    }
+}
