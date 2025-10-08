54159ca1fabf8886e44fbb106f497bb90426cfa1
Liu <Unofatira@gmail.com>
2025-10-08 14:38:25 +0700

    update

--- industryClass/absen.java
+++ industryClass/absen.java
@@ -2,7 +2,7 @@ public class absen {
     public static void main(String[] args) {
         for (int i = 1; i <= 3; i++) {
             System.out.println();
-            System.out.println("kelas" + i);
+            System.out.println("kelas " + i);
             for (int j = 1; j <= 10; j++) {
                 
                 if (j == 8) {
