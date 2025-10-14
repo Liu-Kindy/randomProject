602b3e25e7ce1ca537f1eaac34ba36ce0b466f81
Liu <Unofatira@gmail.com>
2025-10-14 17:07:34 +0700

    update

--- Getskill/inputArray2D.java
+++ Getskill/inputArray2D.java
@@ -8,11 +8,20 @@ public class inputArray2D {
         System.out.print("masukkan jumlah kolom: ");
         int kolom=scanner.nextInt();
         int[][] array2D = new int[baris][kolom];
-        System.out.println("masukkan elemen array: ");
+        
         for (int i = 0; i < baris; i++) {
+            System.out.println("baris elemen ke-" + i);
             for (int j = 0; j < kolom; j++) {
+                System.out.print("masukkan elemen ke-" + j + ": ");
                 array2D[i][j] = scanner.nextInt();
             }
+        }
+        System.out.println("--tabel matrix--");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print(array2D[i][j] + " ");
+            }
+            System.out.println();
         }
 
 
