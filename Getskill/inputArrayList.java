d228e6df413e5e9c067c98e0d3f590bbc19e634b
Liu <Unofatira@gmail.com>
2025-10-22 15:24:24 +0700

    update

--- Getskill/inputArrayList.java
+++ Getskill/inputArrayList.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 
 public class inputArrayList {
     public static void main(String[] args) {
-        ArrayList nama = new ArrayList(5);
+        ArrayList<String> nama = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        System.out.println("==Masukan 5 nama teman==");
         for (int i = 0; i < 5; i++) {
-        System.out.println("masukan nama : ");
+        System.out.print("masukan nama teman ke-"+(i+1)+": ");
         String inputNama = scanner.nextLine();
-        }
-        for (int i = 0; i < 5; i++) {
         nama.add(inputNama);
         }
         
         
+        System.out.println("\n==Daftar nama teman yang dimasukan==");
         for (int i = 0; i < nama.size(); i++) {
-            System.out.println("Nama yang dimasukan: " + nama.get(i));
+            System.out.println("Nama yang dimasukan ke-"+(i+1)+": " + nama.get(i));
 
         }
         
