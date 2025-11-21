cd5bd1aa77742e7fac5197ffdd703246f1653b36
Liu <Unofatira@gmail.com>
2025-11-21 14:17:00 +0700

    add

---
+++ Getskill/abcINC.java
@@ -0,0 +1,11 @@
+public class abcINC {
+    public static void main(String[] args) {
+        for(char i='A'; i <= 'Z'; i++){
+            System.out.print(i+ " ");
+        }
+        System.out.println();
+        for(char i='Z'; i >= 'A'; i--){
+            System.out.print(i+ " ");
+        }
+    }
+}
---
+++ Getskill/lima0ke01.java
@@ -0,0 +1,7 @@
+public class lima0ke01 {
+    public static void main(String[] args) {
+        for (int i = 50; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
